1bbd611e674e6af34f08df99f8d7efc55f6cc48d
Akruzen <1omkarphadke@gmail.com>
2023-06-08 23:10:55 +0530

    Added navbar_art.xml and navbar

--- app/src/main/java/com/akruzen/checklance/MainActivity.java
+++ app/src/main/java/com/akruzen/checklance/MainActivity.java
@@ -2,13 +2,24 @@ package com.akruzen.checklance;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+
+    public void openNavDrawer (View view) {
+        drawerLayout.setDrawerTitle(GravityCompat.START, "Options");
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
     public void addBankFAB (View view) {
         Intent intent = new Intent(this, AddBankActivity.class);
         startActivity(intent);
@@ -18,5 +29,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout = findViewById(R.id.drawerLayout);
     }
 }
