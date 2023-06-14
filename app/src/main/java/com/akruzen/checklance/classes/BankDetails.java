b0e469abcae232e62e66c0b75d6aa28900c5dd8e
Akruzen <1omkarphadke@gmail.com>
2023-06-14 21:49:45 +0530

    Created bank details class

---
+++ app/src/main/java/com/akruzen/checklance/classes/BankDetails.java
@@ -0,0 +1,20 @@
+package com.akruzen.checklance.classes;
+
+import java.util.List;
+
+public class BankDetails {
+
+    int accNo;
+    String bank;
+    // Sender's names, Credit keywords, Debit keywords
+    List<String> sender, credKeys, debKeys;
+    double currBal;
+
+    BankDetails (int accNo, String bank, List<String> sender,
+                List<String> credKeys, List<String> debKeys, double currBal) {
+        this.accNo = accNo; this.bank = bank;
+        this.sender = sender; this.credKeys = credKeys;
+        this.debKeys = debKeys; this.currBal = currBal;
+    }
+
+}
